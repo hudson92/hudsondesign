93554c15a0eb82ef03ed908695791d709f7cd8cb
Sam H <samhudson1992@gmail.com>
2019-12-19 16:10:06 -0700

    updates including js attempt

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+//Modules
+import Menu from './modules/menu';
+
+//Instantiate a new object using our modules/classes
+var menu = new Menu();
+
+
---
+++ js/modules/menu.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+class Menu {
+	// 1. describe and create/initiate our object
+	constructor() {
+		this.navLink = $(".nav-item");
+	}
+	// 2. list of events
+	events() {
+		this.navLink.on("click", this.highlightNav.bind(this));
+	}
+
+	// 3. where our methods (function, action...) live
+	highlightNav() {
+		this.navLink.addClass("active");
+	}
+}
+
+export default Menu;
